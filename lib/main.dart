6c17ecdc758825d5778c6089a53dff7b30550a58
adekanyetomie <adekanyetomy@gmail.com>
2019-10-10 20:28:03 +0100

    completed the app

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo List',
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Color.fromARGB(200, 25, 20, 255)
+          ),
+      ),
       home: new TodoList()
     );
   }
@@ -20,25 +25,51 @@ class TodoList extends StatefulWidget{
 class TodoListState extends State<TodoList>{
   List todoElements = [];
 
-  void addTodoElements(){
-    setState(() {
-      int index = todoElements.length;
-      todoElements.add('Item ' + index.toString());
-    });
+  void addTodoElements(task){
+    if (task.length >0){}
+    setState(() => todoElements.add(task));
+  }
+  void modalDeleteElement(index){
+    showDialog(
+      context: context,
+      builder: (BuildContext context){
+        return new AlertDialog(
+          title: new Text('Have you completed "${todoElements[index]}" task?'),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text('Cancel?'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            new FlatButton(
+              child: new Text('Completed'),
+              onPressed: () {
+                deleteTodoElements(index);
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      }
+    );
+  }
+  void deleteTodoElements(index){
+    setState(()=> todoElements.removeAt(index));
+
   }
   Widget buildTodoList(){
     return new ListView.builder(
       itemBuilder: (context, index){
         if (index < todoElements.length){
-          return buildTodoElement(todoElements[index]);
+          return buildTodoElement(todoElements[index], index);
         }
       }
 
     );
   }
-  Widget buildTodoElement( todo){
+  Widget buildTodoElement( todo, index){
     return new ListTile(
       title: new Text(todo),
+      onTap: () => modalDeleteElement(index),
     );
   }
 
@@ -50,10 +81,35 @@ class TodoListState extends State<TodoList>{
         ),
         body: buildTodoList(),
         floatingActionButton: new FloatingActionButton(
-          onPressed: addTodoElements,
+          onPressed: todoNewScreen,
           tooltip: 'Add Task',
           child: new Icon(Icons.add),
         ),
+    );
+  }
+  void todoNewScreen(){
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context){
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Add a new task') ,
+              ),
+              body: new TextField(
+                autocorrect: true,
+                autofocus: true,
+                onSubmitted: (value){
+                  addTodoElements(value);
+                  Navigator.pop(context);
+                  decoration: new InputDecoration(
+                    hintText: 'Enter your task here ...',
+                    contentPadding: const EdgeInsets.all(12.0)
+                  );
+                },
+                ),
+            );
+        }
+      )
     );
   }
 }
